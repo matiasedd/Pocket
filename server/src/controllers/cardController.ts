a84740a62fcf4c8f108fd6d6c4d13b2ab2b6f8be
murilo9 <murilohenriquematias@gmail.com>
2021-04-12 22:28:32 -0300

    implemented AirBnB ESLint

--- server/src/controllers/cardController.ts
+++ server/src/controllers/cardController.ts
@@ -1,21 +1,21 @@
-import { Request, Response } from "express";
-import { v4 as uuidv4 } from "uuid";
-import database from "../database";
+import { Request, Response } from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import database from '../database';
 
 const card = {
   listAll: async (req: Request, res: Response) => {
-    const query = await database("cards");
+    const query = await database('cards');
     res.json(query);
   },
   listCard: async (req: Request, res: Response) => {
     const { userd_id, card_id } = req.params;
-    const query = await database("cards").where({ card_id, userd_id });
+    const query = await database('cards').where({ card_id, userd_id });
     res.json(query);
   },
   add: async (req: Request, res: Response) => {
     const card_id = uuidv4();
     try {
-      await database("cards").insert({ ...req.body, card_id });
+      await database('cards').insert({ ...req.body, card_id });
       res.sendStatus(201);
     } catch (err) {
       res.send(err);
@@ -24,7 +24,7 @@ const card = {
   delete: async (req: Request, res: Response) => {
     const { user_id, card_id } = req.params;
     try {
-      await database("cards").where({ user_id, card_id }).del();
+      await database('cards').where({ user_id, card_id }).del();
       res.sendStatus(200);
     } catch (err) {
       res.send(err);
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,29 +1,29 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import user from "./controllers/userController";
-import card from "./controllers/cardController";
-import transaction from "./controllers/transactionController";
+import user from './controllers/userController';
+import card from './controllers/cardController';
+import transaction from './controllers/transactionController';
 
 const router = Router();
 
-router.get("/users", user.listAll);
-router.get("/users/:user_id", user.listUser);
+router.get('/users', user.listAll);
+router.get('/users/:user_id', user.listUser);
 
-router.get("/users/:user_id/cards", card.listAll);
-router.get("/users/:user_id/cards/:card_id", card.listCard);
+router.get('/users/:user_id/cards', card.listAll);
+router.get('/users/:user_id/cards/:card_id', card.listCard);
 
-router.get("/users/:user_id/cards/:card_id/transactions", transaction.listAll);
-router.get("/users/:user_id/cards/:card_id/transactions/:transaction_id", transaction.listTransaction);
+router.get('/users/:user_id/cards/:card_id/transactions', transaction.listAll);
+router.get('/users/:user_id/cards/:card_id/transactions/:transaction_id', transaction.listTransaction);
 
-router.post("/users", user.add);
-router.post("/users/:user_id/cards", card.add);
-router.post("/users/:user_id/cards/:card_id/transactions", transaction.add);
+router.post('/users', user.add);
+router.post('/users/:user_id/cards', card.add);
+router.post('/users/:user_id/cards/:card_id/transactions', transaction.add);
 
-router.put("/users/:user_id", user.update);
-router.put(  "/users/:user_id/cards/:card_id/transactions/:transaction_id", transaction.update);
+router.put('/users/:user_id', user.update);
+router.put('/users/:user_id/cards/:card_id/transactions/:transaction_id', transaction.update);
 
-router.delete("/users/:user_id", user.delete);
-router.delete("/users/:user_id/cards/:card_id", card.delete);
-router.delete("/users/:user_id/cards/:card_id/transactions/:transaction_id", transaction.delete);
+router.delete('/users/:user_id', user.delete);
+router.delete('/users/:user_id/cards/:card_id', card.delete);
+router.delete('/users/:user_id/cards/:card_id/transactions/:transaction_id', transaction.delete);
 
 module.exports = router;
