31cbc71e1c8fcc85878d00218e9738748fb3dd87
murilo9 <murilohenriquematias@gmail.com>
2021-04-14 18:02:16 -0300

    api: removed card entity

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 
 import user from './controllers/userController';
-import card from './controllers/cardController';
 import transaction from './controllers/transactionController';
 
 const router = Router();
@@ -9,21 +8,15 @@ const router = Router();
 router.get('/users', user.listAll);
 router.get('/users/:user_id', user.listUser);
 
-router.get('/users/:user_id/cards', card.listAll);
-router.get('/users/:user_id/cards/:card_id', card.listCard);
-
-router.get('/users/:user_id/cards/:card_id/transactions', transaction.listAll);
-router.get('/users/:user_id/cards/:card_id/transactions/:transaction_id', transaction.listTransaction);
+router.get('/users/:user_id/transactions', transaction.listAll);
+router.get('/users/:user_id/transactions/:transaction_id', transaction.listTransaction);
 
 router.post('/users', user.add);
-router.post('/users/:user_id/cards', card.add);
-router.post('/users/:user_id/cards/:card_id/transactions', transaction.add);
 
 router.put('/users/:user_id', user.update);
-router.put('/users/:user_id/cards/:card_id/transactions/:transaction_id', transaction.update);
+router.put('/users/:user_id/transactions/:transaction_id', transaction.update);
 
 router.delete('/users/:user_id', user.delete);
-router.delete('/users/:user_id/cards/:card_id', card.delete);
-router.delete('/users/:user_id/cards/:card_id/transactions/:transaction_id', transaction.delete);
+router.delete('/users/:user_id/transactions/:transaction_id', transaction.delete);
 
 module.exports = router;
