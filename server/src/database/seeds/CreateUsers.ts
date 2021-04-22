8f76a72255304bf27d13eaa16a454873e51240ea
murilo9 <murilohenriquematias@gmail.com>
2021-04-21 22:24:07 -0300

    fixed CreateUsers seed

--- server/src/database/seeds/CreateUsers.ts
+++ server/src/database/seeds/CreateUsers.ts
@@ -13,9 +13,10 @@ const users = [
 export async function createUsers() {
   const userRepo = new UserRepository();
   const userExists = await userRepo.readByEmail(users[0].email);
-  if (!userExists) {
+  if (!userExists.length) {
     console.log('User does not exist. Creating...');
-    await userRepo.insert(users[0]);
+    const user = await userRepo.insert(users[0]);
+    console.log(user);
   } else {
     console.log('User already exists:');
     console.log(userExists);
