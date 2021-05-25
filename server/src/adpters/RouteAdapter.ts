c07c176cf54cdf3108c97c1fb9b510a3d2f4d947
murilo9 <murilohenriquematias@gmail.com>
2021-05-25 19:36:45 -0300

    route adapter puts validator response body into controller handle request body

--- server/src/adpters/RouteAdapter.ts
+++ server/src/adpters/RouteAdapter.ts
@@ -11,6 +11,7 @@ export const makeRoute = (controller: BaseController) => (
   async (request: Request, response: Response) => {
     let validate = <ErrorHttpResponse>{};
     let handle = <HttpResponse>{};
+    // Realiza a validação caso a controller seja assertive
     if (controller instanceof BaseAssertiveController) {
       try {
         validate = await controller.validate(request as HttpRequest);
@@ -25,7 +26,9 @@ export const makeRoute = (controller: BaseController) => (
         },
       };
     }
+    // Após a validação, executa o método handle da controller
     if (validate.statusCode >= 200 && validate.statusCode < 299) {
+      request.body = { ...request.body, ...validate.body };
       try {
         handle = await controller.handle(request);
       } catch (error) {
