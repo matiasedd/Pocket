fd6e02f982d57e819474c58bfaf2d26d0e898062
murilo9 <murilohenriquematias@gmail.com>
2021-05-25 22:07:15 -0300

    implemented DELETE User endpoint

--- server/src/routes/User.ts
+++ server/src/routes/User.ts
@@ -10,6 +10,8 @@ import { addUserValidator } from '../validators/users/AddUser';
 import { BaseRouter } from './base';
 import { UpdateUserController } from '../controllers/user/UpdateUser';
 import { updateUserValidator } from '../validators/users/UpdateUser';
+import { DeleteUserController } from '../controllers/user/DeleteUser';
+import { deleteUserValidator } from '../validators/users/DeleteUser';
 
 export class UserRoutes implements BaseRouter {
   // TODO: change 'routes' to a better, most descriptive name
@@ -18,5 +20,6 @@ export class UserRoutes implements BaseRouter {
     app.get('/users/:userId', auth, makeRoute(new GetUserController(getUserValidator, new UserRepository())));
     app.post('/users', makeRoute(new AddUserController(addUserValidator, new UserRepository())));
     app.put('/users/:userId', auth, makeRoute(new UpdateUserController(updateUserValidator, new UserRepository())));
+    app.delete('/users/:userId', auth, makeRoute(new DeleteUserController(deleteUserValidator, new UserRepository())));
   }
 }
