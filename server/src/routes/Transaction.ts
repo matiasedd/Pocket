3dfaa9216d5c4211be2a08e0626001ff5afdd7b4
murilo9 <murilohenriquematias@gmail.com>
2021-05-29 00:44:24 -0300

    impleented update transaction endpoint

--- server/src/routes/Transaction.ts
+++ server/src/routes/Transaction.ts
@@ -3,12 +3,14 @@ import { makeRoute } from '../adpters/RouteAdapter';
 import { AddTransactionController } from '../controllers/transaction/AddTransaction';
 import { GetTransactionController } from '../controllers/transaction/GetTransaction';
 import { GetUserTransactionsController } from '../controllers/transaction/GetUserTransactions';
+import { UpdateTransactionController } from '../controllers/transaction/UpdateTransaction';
 import { auth } from '../middlewares/Auth';
 import { TransactionRepository } from '../repositories/Transaction';
 import { UserRepository } from '../repositories/User';
 import { AddTransactionValidator } from '../validators/transactions/AddTransaction';
 import { GetTransactionValidator } from '../validators/transactions/GetTransaction';
 import { GetUserTransactionsValidator } from '../validators/transactions/GetUserTransactions';
+import { UpdateTransactionValidator } from '../validators/transactions/UpdateTransaction';
 import { BaseRouter } from './base';
 
 export class TransactionRoutes implements BaseRouter {
@@ -16,5 +18,6 @@ export class TransactionRoutes implements BaseRouter {
     app.get('/users/:userId/transactions', auth, makeRoute(new GetUserTransactionsController(new GetUserTransactionsValidator(new UserRepository()), new TransactionRepository())));
     app.get('/transactions/:transactionId', auth, makeRoute(new GetTransactionController(new GetTransactionValidator(new UserRepository()), new TransactionRepository())));
     app.post('/transactions', auth, makeRoute(new AddTransactionController(new AddTransactionValidator(new UserRepository()), new TransactionRepository())));
+    app.put('/transactions/:transactionId', auth, makeRoute(new UpdateTransactionController(new UpdateTransactionValidator(new UserRepository(), new TransactionRepository()), new TransactionRepository())));
   }
 }
---
+++ server/src/validators/transactions/UpdateTransaction.ts
@@ -0,0 +1,62 @@
+import { HttpRequest } from '../../protocols/HttpRequest';
+import { HttpResponse } from '../../protocols/HttpResponse';
+import { TransactionRepository } from '../../repositories/Transaction';
+import { UserRepository } from '../../repositories/User';
+import { ControllerValidator } from '../Base';
+
+export class UpdateTransactionValidator extends ControllerValidator {
+  private userRepository: UserRepository;
+
+  private transactionRepository: TransactionRepository;
+
+  constructor(userRepository: UserRepository, transactionRepository: TransactionRepository) {
+    super();
+    this.userRepository = userRepository;
+    this.transactionRepository = transactionRepository;
+  }
+
+  async validate(request: HttpRequest): Promise<HttpResponse> {
+    const { userId } = request.body;
+    const { transactionId } = request.params;
+    const { requestUserId } = request.userId;
+    const userExists = await this.userRepository.read(userId);
+    const transactionExists = await this.transactionRepository.read(transactionId);
+    // Se o usuário existir
+    if (userExists) {
+      // Se o usuário tiver o mesmo id do requester
+      if (userExists.id === requestUserId) {
+        // Se a transação existir
+        if (transactionExists) {
+          // Se a transação pertence ao usuário
+          if (transactionExists.userId === userId) {
+            // TODO: verificar se os atributos a serem atualizados da transaction são válidos
+            return {
+              statusCode: 200,
+              body: {},
+            };
+          }
+          // Se a transação não pertence ao usuário
+          return {
+            statusCode: 403,
+            body: 'Usuário não é dono do recurso',
+          };
+        }
+        // Se a transação não existir
+        return {
+          statusCode: 404,
+          body: 'Transação não encontrada',
+        };
+      }
+      // Se o usuário não tiver o mesmo id do requester
+      return {
+        statusCode: 401,
+        body: 'Usuário não é dono do recurso',
+      };
+    }
+    // Se o usuário não existir
+    return {
+      statusCode: 404,
+      body: 'Usuário não encontrado',
+    };
+  }
+}
