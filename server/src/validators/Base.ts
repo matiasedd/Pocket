a68171139619bb9ec0c2091444004723fdbf35cb
murilo9 <murilohenriquematias@gmail.com>
2021-06-13 18:49:44 -0300

    added base controller validator mock cla

--- server/src/validators/Base.ts
+++ server/src/validators/Base.ts
@@ -1,6 +1,6 @@
+/* eslint-disable max-classes-per-file */
 import { HttpRequest } from '../protocols/HttpRequest';
 import { HttpResponse } from '../protocols/HttpResponse';
-import { BaseRespository } from '../repositories/Base';
 
 export class ControllerValidator {
   public async validate(request: HttpRequest): Promise<HttpResponse> {
@@ -8,5 +8,16 @@ export class ControllerValidator {
       statusCode: 200,
       body: {},
     };
+  }
+}
+
+/**
+ * Um mock da classe ControllerValidator que sempre retorna a response passada
+ * no construtor. A finaldiade é ser usado nos testes para evitar o uso de
+ * validators concretos.
+ */
+export class ControllerValidatorMock extends ControllerValidator {
+  public async validate(request: HttpRequest): Promise<HttpResponse> {
+    return Promise.resolve(request as HttpResponse);
   }
 }
