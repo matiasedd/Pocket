239d3a4fe9f4000c5a818edea6e95142c1708533
murilo9 <murilohenriquematias@gmail.com>
2021-07-03 00:02:49 -0300

    login returns user id

--- server/src/controllers/login/Login.ts
+++ server/src/controllers/login/Login.ts
@@ -16,7 +16,6 @@ export class LoginController extends BaseAssertiveController {
   async handle(request: HttpRequest): Promise<HttpResponse> {
     const { email } = request.body;
     const user = await this.userRepository.readByEmail(email);
-    console.log('env', process.env.JWT_SECRET);
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
       expiresIn: 3600, // Expira em 1 h
     });
@@ -24,6 +23,7 @@ export class LoginController extends BaseAssertiveController {
       statusCode: 200,
       body: {
         token,
+        userId: user.id,
       },
     };
   }
